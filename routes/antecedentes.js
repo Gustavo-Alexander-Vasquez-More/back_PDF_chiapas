1d2587f17a19af840822880a32c8c0dc60b38061
Gustavo alexander Vasquez More <123557057+Gustavo-Alexander-Vasquez-More@users.noreply.github.com>
2023-10-22 12:20:30 -0300

    act

--- routes/antecedentes.js
+++ routes/antecedentes.js
@@ -11,7 +11,7 @@ const upload = multer({ dest:'uploads/'});
 const antecedentes_router=Router()
 antecedentes_router.get('/' , read),
 antecedentes_router.get('/todos', readAll)
-antecedentes_router.post('/create',folioExist,  upload.fields([{ name: 'foto' }, { name: 'huella' }, { name: 'qr' }]), create)
+antecedentes_router.post('/create',  upload.fields([{ name: 'foto' }, { name: 'huella' }, { name: 'qr' }]), folioExist, create)
 antecedentes_router.put('/update/:folio', update)
 antecedentes_router.delete('/delete', destroy)
 antecedentes_router.get('/author', readAuth)
