48741a17431553e4d2ccbcafcfeb8e4bc62f7a37
Gustavo alexander Vasquez More <123557057+Gustavo-Alexander-Vasquez-More@users.noreply.github.com>
2023-10-22 12:01:25 -0300

    middleware folio exist

---
+++ middlewares/folioExist.js
@@ -0,0 +1,17 @@
+import Antecedentes from "../models/Antecedentes.js";
+export default async function(req,res,next) {
+try {
+    const folio=await Antecedentes.findOne({folio:req.body.folio})
+    if(folio){
+        return res.status(400).json({
+            success: false,
+            message: 'Este Folio ya esta existe'
+    })
+}else {
+    return next()
+}
+} catch (error) {
+  console.log(error);
+}
+
+}
--- routes/antecedentes.js
+++ routes/antecedentes.js
@@ -5,12 +5,13 @@ import create from '../controllers/antecedentes/create.js'
 import destroy from '../controllers/antecedentes/destroy.js'
 import update from '../controllers/antecedentes/update.js'
 import readAuth from "../controllers/antecedentes/readAuth.js";
+import folioExist from "../middlewares/folioExist.js";
 import multer from "multer";
 const upload = multer({ dest:'uploads/'});
 const antecedentes_router=Router()
 antecedentes_router.get('/' , read),
 antecedentes_router.get('/todos', readAll)
-antecedentes_router.post('/create', upload.fields([{ name: 'foto' }, { name: 'huella' }, { name: 'qr' }]), create)
+antecedentes_router.post('/create',folioExist,  upload.fields([{ name: 'foto' }, { name: 'huella' }, { name: 'qr' }]), create)
 antecedentes_router.put('/update/:folio', update)
 antecedentes_router.delete('/delete', destroy)
 antecedentes_router.get('/author', readAuth)
