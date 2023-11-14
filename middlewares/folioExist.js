2cf508c66d70b5e43a8a5e70094f4981bc5d5011
GustavoVasquez <123557057+Gustavo-Alexander-Vasquez-More@users.noreply.github.com>
2023-11-14 11:32:20 -0300

    Update folioExist.js

--- middlewares/folioExist.js
+++ middlewares/folioExist.js
@@ -1,18 +1,23 @@
 import Antecedentes from "../models/Antecedentes.js";
 export default async function(req,res,next) {
-try {
-    const folio=await Antecedentes.findOne({folio:req.body.folio})
-    if(folio){
-        return res.status(400).json({
-            response:null,
-            success: false,
-            message: 'Este Folio ya esta existe'
-    })
-}else {
-    return next()
-}
-} catch (error) {
-  console.log(error);
-}
-
+  try {
+    const folio = await Antecedentes.findOne({ folio: req.body.folio });
+    
+    if (folio) {
+      return res.status(400).json({
+        response: null,
+        success: false,
+        message: 'Este Folio ya existe'
+      });
+    } else {
+      return next();
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      response: null,
+      success: false,
+      message: 'Error interno del servidor'
+    });
+  }
 }
