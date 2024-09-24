2277b6a8aa26b34442739a292a1baae7460d9775
GustavoVasquez <tavovm24@hotmail.com>
2024-09-24 13:46:24 -0300

    sad

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import admins_router from './admins.js';
 import antecedentes_router from './antecedentes.js';
+import registro_folios_router from './registro_folios.js';
+import registro_usurios_router from './registro_usuarios.js';
 let router = express.Router()
 
 router.get('/', function(req, res, next) {
@@ -10,4 +12,6 @@ router.get('/', function(req, res, next) {
 
 router.use('/admins', admins_router)
 router.use('/antecedentes', antecedentes_router)
+router.use('/registro_folios', registro_folios_router)
+router.use('/registro_usuarios', registro_usurios_router)
 export default router
